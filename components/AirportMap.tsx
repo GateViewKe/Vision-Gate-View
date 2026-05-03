c1d952453a3eb43af29d3372b38507b20e79330f
Levy Carlos <78299775+Levyc574@users.noreply.github.com>
2026-05-03 05:25:59 +0300

    Update handleClick to specify HTMLCanvasElement type

--- components/AirportMap.tsx
+++ components/AirportMap.tsx
@@ -201,7 +201,7 @@ export default function AirportMap({ floorPlan, playerX, playerY, onSelectPOI }:
     return () => ro.disconnect()
   }, [])
 
-  function handleClick(e: React.MouseEvent) {
+  function handleClick(e: React.MouseEvent<HTMLCanvasElement>) {
     const canvas = canvasRef.current!
     const r = canvas.getBoundingClientRect()
     const cx = e.clientX - r.left
